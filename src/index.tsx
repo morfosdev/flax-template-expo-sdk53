488383e7e999a94bbcddedc1031c050e3eae5617
Carlos A. <carlosvsk@gmail.com>
2025-05-20 16:51:06 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -362,7 +362,7 @@ xmlns="http://www.w3.org/2000/svg"
             functions:[async (...args) =>
  functions.funcGroup({ args, pass:{
  arrFunctions: [() => {
-console.log({ testList: tools.base.jsvals.argReader("#0.0") });
+console.log({ testList: tools.base});
 
   const path = "all.toggles.checkbox1";
   const currState = tools.getCtData(path);
