2dca09c51ad88d48eacf126e92bab28f93e98701
Carlos A. <carlosvsk@gmail.com>
2025-05-20 16:01:21 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -54,7 +54,7 @@
 
             functions:[async (...args) =>
  functions.funcGroup({ args, pass:{
- arrFunctions: [()=>"all.toggles.box1"]
+ arrFunctions: [() => [ "all.toggles.box1", "==", true ]]
  , trigger: 'on listen'
 }})],            childrenItems:[
         
