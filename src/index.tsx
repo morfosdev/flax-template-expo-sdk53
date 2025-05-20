ee23b39027e98727355f3234fde7fb6c220a62d7
Carlos A. <carlosvsk@gmail.com>
2025-05-20 14:56:15 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -136,7 +136,7 @@ xmlns="http://www.w3.org/2000/svg"
             args,
           }}/>
         , (...args:any) => <Elements.ScrollBar pass={{
-            styles: [`{ backgroundColor: "red" }`],
+            styles: [`{ backgroundColor: "red", width: "100%" }`],
             arrProps: [],
             arrItems: [
         (...args:any) => <Elements.IptTxtEdit pass={{
